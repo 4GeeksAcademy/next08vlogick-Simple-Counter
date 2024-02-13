956e68a9a94f60f07e549a64134d365c1078231e
wilson isaac castro chavez <148562958+Next08vlogick@users.noreply.github.com>
2024-02-13 18:25:53 +0000

    proyecto casi terminado

--- src/js/component/Clock.js
+++ src/js/component/Clock.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
 
-const Clock = () => {
+const Clock = (props) => {
   return (
-    <div>
-        <i className="fa-solid fa-clock"></i>
+    <div className=" container-fluid row justify-content-center position-absolute top-50 start-50 translate-middle    border-danger-">
+      <div class="gearbox">
+        <div class="overlay"></div>
+        <div class="gear one">
+          <div class="gear-inner">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+          </div>
+        </div>
+        <div class="gear two">
+          <div class="gear-inner">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+          </div>
+        </div>
+        <div class="gear three">
+          <div class="gear-inner">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+          </div>
+        </div>
+        <div class="gear four large">
+          <div class="gear-inner">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="cajitas me-1 rounded col-2">
+        <div className="numeros00">{props.seconds}</div>
+      </div>
+
+      <div className="cajitas me-1 rounded col-2">
+        <div className="numeros00">{props.seconds}</div>
+      </div>
+
+      <div className="cajitas me-1 rounded col-2">
+        <div className="numeros00">{props.seconds}</div>
+      </div>
+
+      <div className="cajitas me-1 rounded col-2">
+        <div className="numeros00">{props.seconds}</div>
+      </div>
+
+      <div className="cajitas me-1 rounded col-2">
+        <div className="numeros00">{props.seconds}</div>
+      </div>
+
+      <div className="cajitas me-1 rounded col-2">
+        <div className="numeros00 ">{props.seconds}</div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Clock;
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,16 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Clock from "../js/component/Clock";
 
 //render your react application
 let segundos = 0;
 setInterval(() => {
-  ReactDOM.render(<Home seconds={segundos} />, document.querySelector("#app"));
+  ReactDOM.render(<Clock seconds={segundos} />, document.querySelector("#app"));
   console.log("segundos:", segundos);
   segundos++;
 }, 1000);
-
